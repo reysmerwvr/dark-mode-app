c3de37ffcf347e03299691ba4790dff659da1fb2
Reysmer Valle <reysmerwvr@gmail.com>
2020-06-05 18:40:45 -0500

    Updates

---
+++ precache-manifest.06669d1a7b5420c6c7452c9eaa9df2e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc3e6701af3ae0319788fabd5cb52de",
+    "url": "/dark-mode-app/index.html"
+  },
+  {
+    "revision": "be2c067575d722a20a68",
+    "url": "/dark-mode-app/static/css/main.3ab73c8c.chunk.css"
+  },
+  {
+    "revision": "94240428c6f265a2398b",
+    "url": "/dark-mode-app/static/js/2.fa73ddae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dark-mode-app/static/js/2.fa73ddae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2c067575d722a20a68",
+    "url": "/dark-mode-app/static/js/main.91b956e7.chunk.js"
+  },
+  {
+    "revision": "5a4d3e120056d5fb4df3",
+    "url": "/dark-mode-app/static/js/runtime-main.88f31cb4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dark-mode-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
